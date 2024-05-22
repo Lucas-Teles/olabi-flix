7bf089bb404ccc8dd3093ccecfedbdfd2f0bf57a
Lucas Teles <lucasouzateles@gmail.com>
2024-05-22 20:41:10 -0300

    :tada: First commit

---
+++ src/main/java/com/olabi/olabiflix/OlabiflixApplication.java
@@ -0,0 +1,13 @@
+package com.olabi.olabiflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlabiflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlabiflixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/olabi/olabiflix/OlabiflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olabi.olabiflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlabiflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
