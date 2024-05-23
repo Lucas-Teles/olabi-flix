da5efa56ec9089cd4fe6618b7a9c54c0de251640
Lucas Teles <lucasouzateles@gmail.com>
2024-05-23 19:14:52 -0300

    HELLO MUNDÃO

--- src/main/java/com/olabi/olabiflix/OlabiflixApplication.java
+++ src/main/java/com/olabi/olabiflix/OlabiflixApplication.java
@@ -2,12 +2,23 @@ package com.olabi.olabiflix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+
 public class OlabiflixApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OlabiflixApplication.class, args);
 	}
+
+	@GetMapping("/hello")
+	public String hello(){
+		return "Salve, mundão!!";
+	}
+
+
 
 }
